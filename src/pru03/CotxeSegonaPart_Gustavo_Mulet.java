35449bf9eafe294058d80743eaa8647c9a5083f0
gfernandez09 <47225340+gfernandez09@users.noreply.github.com>
2020-12-18 13:49:31 +0100

    el primer commit

--- src/pru03/CotxeSegonaPart_Gustavo_Mulet.java
+++ src/pru03/CotxeSegonaPart_Gustavo_Mulet.java
@@ -5,6 +5,12 @@ Created by: Gusito
 Date: 11/12/2020
 Description: 
 */
+enum Maleter{
+    Obert,Tancat
+}
+enum Portes{
+    Obertes,Tancades
+}
 enum marxaAutomatic{
     F, N, R
 }
@@ -12,13 +18,16 @@ enum marxesManual {
     R, N, Primera, Segona, Tercera, Quarta, Quinta, Sexta
 }
 public class CotxeSegonaPart_Gustavo_Mulet extends Coche_Gustavo_Mulet {
+    protected Maleter maleter;
+    protected Portes portes;
     protected marxesManual marxaManual;
     protected marxaAutomatic marxaAutomatica;
     public CotxeSegonaPart_Gustavo_Mulet(String marca, String model, TipusCanvi tipusCanvi, EstatsMotorCotxe estatCotxe, marxaAutomatic marxaAutomatica, marxesManual marxaManual) {
         super(marca, model, tipusCanvi, estatCotxe);
         this.marxaManual = marxaManual;
         this.marxaAutomatica = marxaAutomatica;
-
+        this.portes = portes;
+        this.maleter = maleter;
     }
 
     //Mètode per pujar/baixar marxes al tipus de canvi manual. Lo primer que feim es posar la marxa manual a R i desprès pujar fins a Sexta i desprès baixar una
@@ -82,7 +91,37 @@ public class CotxeSegonaPart_Gustavo_Mulet extends Coche_Gustavo_Mulet {
             throw new Exception("El cotxe ha d'estar encès ");
         }
     }
+    public void obrirMaleter() throws Exception{
+        if(this.maleter == maleter.Tancat){
+            this.maleter = maleter.Obert;
+        }else{
+            throw new Exception("Tens el maleter obert");
+        }
+    }
+    public void tencarMaleter() throws Exception{
+        if(this.maleter == maleter.Obert){
+            this.maleter = maleter.Tancat;
+        }else{
+            throw new Exception("Tens el maleter tancat");
+        }
+    }
+    public void obrirPortes() throws Exception{
+        if(this.portes == portes.Tancades){
+            this.portes = portes.Obertes;
+        }else{
+            throw new Exception("Tens les portes obertes");
+        }
+    }
+    public void tencarPortes() throws Exception{
+        if(this.portes == portes.Obertes){
+            this.portes = portes.Tancades;
+        }else{
+            throw new Exception("Les portes ja estan tancades");
+        }
+    }
     //Getters
+    public Portes getPortes(){ return this.portes; }
+    public Maleter getMaleter(){ return this.maleter;}
     public marxesManual getmarxaManual(){ return marxaManual; }
     public marxaAutomatic getmarxaAutomatic(){ return marxaAutomatica;
     }
